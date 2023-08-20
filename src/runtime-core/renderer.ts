8f993e67b8038e44504aca7c8b7b8cc4ff64c648
hato <fansintuesan@gmail.com>
2023-08-20 20:45:49 +0800

    [Day 14] runtime-core比對新舊虛擬dom

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,5 +1,5 @@
 import { RenderOptions } from "../runtime-dom";
-import { Text, VNode } from "./vnode";
+import { Text, Fragment, VNode, ShapeFlags, isSameVnode } from "./vnode";
 
 export const createRenderer = (renderOptions: RenderOptions) => {
   const {
@@ -14,10 +14,6 @@ export const createRenderer = (renderOptions: RenderOptions) => {
     patchProp: hostPatchProp
   } = renderOptions;
 
-  const unmount = (vnode: VNode) => {
-    hostRemove(vnode.el);
-  };
-
   const render = (vnode: VNode, container: any) => {
     if (vnode == null) {
       if (container._vnode) unmount(container._vnode);
@@ -25,6 +21,34 @@ export const createRenderer = (renderOptions: RenderOptions) => {
       patch(container._vnode || null, vnode, container);
     }
     container._vnode = vnode;
+  };
+
+  const unmount = (vnode: VNode) => {
+    hostRemove(vnode.el);
+  };
+
+  const patch = (oldNode: VNode, newNode: VNode, container: HTMLElement, anchor: HTMLElement | Text = null) => {
+    if (oldNode === newNode) return;
+
+    if (oldNode && !isSameVnode(oldNode, newNode)) {
+      unmount(oldNode);
+      oldNode = null;
+    }
+
+    // type可為標籤名或Text代表僅渲染純文字，shapeFlag以二進位存儲虛擬dom特徵
+    // （例如children是陣列的HTML元素：10001，參考 shared/index.ts -> ShapeFlags）
+    const { type, shapeFlag } = newNode;
+
+    switch (type) {
+      case Text:
+        processText(oldNode, newNode, container);
+        break;
+      case Fragment:
+        processFragment(oldNode, newNode, container);
+        break;
+      default:
+        if (shapeFlag & ShapeFlags.ELEMENT) processElement(oldNode, newNode, container, anchor);
+    }
   };
 
   return { render };
--- src/runtime-core/vnode.ts
+++ src/runtime-core/vnode.ts
@@ -1,4 +1,5 @@
 export const Text = Symbol('Text');
+export const Fragment = Symbol('Fragment');
 
 export interface VNode {
   type: string | symbol,
@@ -22,4 +23,33 @@ export const enum ShapeFlags {
   COMPONENT_SHOULD_KEEP_ALIVE = 1 << 8,
   COMPONENT_KEEP_ALIVE = 1 << 9,
   COMPONENT = ShapeFlags.STATEFUL_COMPONENT | ShapeFlags.FUNCTIONAL_COMPONENT
+}
+
+export const createVNode = (type: string | symbol, props: Record<string, any>, children: Array<VNode | string> | string = null) => {
+  const shapeFlag = typeof type === 'string' ? ShapeFlags.ELEMENT : 0;
+
+  const vnode: VNode = {
+    type,
+    props,
+    children,
+    el: null,
+    key: props?.key,
+    __v_isVnode: true,
+    shapeFlag
+  };
+
+  if (children) {
+    let type;
+    if (Array.isArray(children)) type = ShapeFlags.ARRAY_CHILDREN;
+    else {
+      children = String(children);
+      type = ShapeFlags.TEXT_CHILDREN;
+    }
+    vnode.shapeFlag |= type;
+  }
+  return vnode;
+};
+
+export function isSameVnode(oldNode: VNode, newNode: VNode) {
+  return (oldNode.type === newNode.type) && (oldNode.key === newNode.key);
 }
