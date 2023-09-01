3d152aaa637a7608c579e1d45e810a3a11aa0ac5
hato <fansintuesan@gmail.com>
2023-09-01 14:31:04 +0800

    [Day 25]ast抽象語法樹 - 2——解析標籤

--- src/ast/parse.ts
+++ src/ast/parse.ts
@@ -16,4 +16,40 @@ export const parse = (template: string) => {
     children: (string | TagContent)[];
   };
   const textStack: TagContent[] = [{ children: [] }];
+
+  while (index < template.length - 1) {
+    const rest = template.substring(index);
+    // 檢測剩餘文字「開頭」為開始標籤、結束標籤或內文
+    if (startRegExp.test(rest)) {
+      // 捕獲標籤內文字賦值tag
+      const tag = rest.match(startRegExp)[1];
+      const attrsStr = rest.match(startRegExp)[2];
+      const attrs = parseAttrsString(attrsStr);
+
+      tagStack.push(tag);
+      textStack.push({ tag, children: [], attrs });
+      // 標籤文字字數 + 2即為開始標籤字數，將指針後移至開始標籤之後
+      index += tag.length + 2 + (attrsStr ? attrsStr.length : 0);
+    } else if (endRegExp.test(rest)) {
+      const tag = rest.match(endRegExp)[1];
+      if (tag === tagStack[tagStack.length - 1]) {
+        tagStack.pop();
+        const popText = textStack.pop();
+        textStack[textStack.length - 1].children.push(popText);
+      } else {
+        throw new Error(tagStack[tagStack.length - 1] + '標籤未封閉！');
+      }
+      index += tag.length + 3;
+    } else if (wordRegExp.test(rest)) {
+      // 剩餘文字開頭非標籤，結尾為下標籤，意即標籤內容
+      const text = rest.match(wordRegExp)[1];
+      // 檢查標籤內文字是否全為空
+      if (!/^\s+$/.test(text)) {
+        textStack[textStack.length - 1].children = parseTextData(text);
+      }
+      index += text.length;
+    } else {
+      index++;
+    }
+  }
 };
